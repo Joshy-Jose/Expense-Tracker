43000a1603cbdc33f3b266dbcb0c820b7a3f7746
Joshy Jose <josejoshy12@gmail.com>
2021-06-22 10:09:00 +0200

    create first component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,16 @@
+import './ExpenseItem.css';
 function ExpenseItem ()  {
     return (
-        <div>
-        <h2>Helloo</h2>
+        <div className="expense-item">
+            <div>March 28th 2021</div>
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+            </div>
+            <div className="expense-item__price">
+                <h2>$294.55</h2>
+            </div>
         </div>
     )
         
     }
-
 export default ExpenseItem;
