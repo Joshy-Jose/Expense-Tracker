d1c98c1dd39d4bd28ba773369217e92f7b74712d
Joshy Jose <josejoshy12@gmail.com>
2021-06-23 11:49:22 +0200

    state management using useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import ExpenseItem from './components/Expenses/ExpenseItem';
-
+import NewExpense from './components/NewExpense/NewExpense';
 const App = () => {
 
   const expenses = [
@@ -29,26 +29,28 @@ const App = () => {
 
   return (
     <div>   
+      <NewExpense/>
       <ExpenseItem
-      tittle = {expenses[0].title}
+      title = {expenses[0].title}
       amount = {expenses[0].amount}
       date = {expenses[0].date}
       />
       <ExpenseItem
-      tittle = {expenses[1].title}
+      title = {expenses[1].title}
       amount = {expenses[1].amount}
       date = {expenses[1].date}
       />
       <ExpenseItem
-      tittle = {expenses[2].title}
+      title = {expenses[2].title}
       amount = {expenses[2].amount}
       date = {expenses[2].date}
       />
       <ExpenseItem
-      tittle = {expenses[3].title}
+      title = {expenses[3].title}
       amount = {expenses[3].amount}
       date = {expenses[3].date}
       />
+    
     </div>
   );
 }
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 
 const ExpenseItem = (props) =>  {
+    
+   const [title, setTitle] = useState(props.title);
+
+   const changeTitleHandler = () =>
+    {
+      setTitle('updated!!');
+    }
+
+    
     return (
         <div className="expense-item">
 
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.tittle}</h2>
+                <h2>{title}</h2>
             </div>
             <div className="expense-item__price">
                 <h2>{props.amount}</h2>
             </div>
+            <button onClick={changeTitleHandler}>Change Title</button>
         </div>
     );
         
